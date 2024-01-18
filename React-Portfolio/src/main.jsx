c2bc21a8d5c1fb2e4c9b83e8f6062f13b51a3f79
tugwellchristi <tugwellchristi@gmail.com>
2024-01-17 16:19:31 -0800

    updated Readme.md, initialized file structure, app.jsx

---
+++ React-Portfolio/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
