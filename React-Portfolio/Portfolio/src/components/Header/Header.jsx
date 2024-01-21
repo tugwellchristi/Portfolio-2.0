4ff6af46bbdfe8222f316035474bd32dd8be7685
tugwellchristi <tugwellchristi@gmail.com>
2024-01-20 22:08:42 -0800

    still troubleshooting css properties

--- React-Portfolio/Portfolio/src/components/Header/Header.jsx
+++ React-Portfolio/Portfolio/src/components/Header/Header.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import './Header.css'
+// import React from 'react';
+// import Box from '@mui/material/Box';
+// import Typography from '@mui/material/Typography';
 
 
-function Header() {
-    return (
-        <Box className='header-box'>
-            <Typography variant='h3' fontWeight='bold' mb={2}>
-                HI, I'M CHRISTI TUGWELL!
-            </Typography>
-            <Typography variant='h5'>
-                Front End Developer And Web Designer
-            </Typography>
-        </Box>
-    );
-}
+// function HeaderBox() {
+//     return (
+//         <Box
+//             sx={{
+//                 height: '400px',
+//                 width: '100%',
+//                 backgroundImage: 'url()',
+//                 backgroundSize: 'cover',
+//                 backgroundPosition: 'center',
+//                 position: 'fixed',
+//                 zIndex: 1,
+//                 display: 'flex',
+//                 flexDirection: 'column',
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 fontFamily: 'monospace',
+//                 fontWeight: 700,
+//                 letterSpacing: '.3rem',
+//                 color: 'white',
+//                 flexGrow: 1,
+//                 fontSize: '2rem',
+//                 marginTop: '60px',
+//                 textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+//             }}
+//         >
+//             <Typography variant='h3' fontWeight='bold' mb={2}>
+//                 HI, I'M CHRISTI TUGWELL!
+//             </Typography>
+//             <Typography variant='h5'>
+//                 Front End Developer And Web Designer
+//             </Typography>
+//         </Box>
+//     );
+// }
 
-export default Header;
+// export default HeaderBox;
